5e131dba2c94d64ef0ceefab156b87af716a9371
tvallejoss <tvallejos@stgroup.com.ar>
2023-03-25 19:26:14 -0300

    fix: problema con las carteras y el modal solucionado

--- mycriptowiselink/src/componentes/Carteras.jsx
+++ mycriptowiselink/src/componentes/Carteras.jsx
@@ -18,35 +18,31 @@ export const Carteras = ({ crypto, i, setCryptoSearch }) => {
     };
 
     const newModalValue = () => {
+        console.log(
+            "Estado Anterior:",
+            ModalEstado,
+            "Estado Nuevo seteado :",
+            !ModalEstado
+        );
         setModalEstado(!ModalEstado);
     };
 
     return (
-        <div className="Card" onClick={newModalValue}>
-            <div className="cardImage">
-                <img src={crypto.image} alt="" />
+        <>
+            <div className="Card" onClick={newModalValue}>
+                <div className="cardImage">
+                    <img src={crypto.image} alt="" />
+                </div>
+
+                <div className="cardInfo">
+                    <h1>{crypto.name}</h1>
+                </div>
             </div>
-
-            <div className="cardInfo">
-                <h1>{crypto.name}</h1>
-                {/* <p>Current Price: {crypto.current_price}</p>
-                <p>Price Change: {crypto.price_change_24h}</p>
-                <p>
-                    Price Change Percentage:{" "}
-                    {crypto.price_change_percentage_24h}
-                </p>
-                <p>Total Volume: {crypto.total_volume}</p> */}
-            </div>
-
-            {/* <div className="cardButton">
-                <p onClick={AddCrypto}>Add to your wallet </p>
-            </div> */}
-
             <Modal
                 ModalEstado={ModalEstado}
-                setModalEstado={setModalEstado}
+                newModalValue={newModalValue}
                 crypto={crypto}
             />
-        </div>
+        </>
     );
 };
